9137dbc33f59fa7dea2a1637829aacfa6390743d
KelompokPoliPerpus <49446051+KelompokPoliPerpus@users.noreply.github.com>
2019-05-01 23:47:31 +0700

    Delete databaseConfig.js

--- serverperpus/model/databaseConfig.js
+++
@@ -1,8 +0,0 @@
-var mysql = require('mysql');
-var dbconnect = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "dbperpus"
-});
-module.exports = dbconnect
